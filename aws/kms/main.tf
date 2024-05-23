3899c572beb8017a8d4c8d7c0a4ffdad1f01dee8
Federico Barrera Oro <federico.oro@workato.com>
2024-05-23 09:38:18 -0300

    New script to create keys

---
+++ aws/kms/main.tf
@@ -0,0 +1,6 @@
+resource "time_static" "date" {
+}
+
+
+data "aws_default_tags" "opa_default_tags" {}
+
---
+++ aws/kms/provider.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  profile = var.profile
+  default_tags {
+    tags = {
+      Name = var.name
+      OWNER = var.owner
+      Due = local.due
+      Provisioning = "Terraform"
+      Jira = "${terraform.workspace}"
+    }    
+  }  
+  region = var.region
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+}
+
+provider "tls" {}
+
