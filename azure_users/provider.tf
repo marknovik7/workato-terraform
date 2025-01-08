1f7e417789b61f9425fdc93dca67c2c7e2accb41
Mark Grivnin <144285053+marknovik7@users.noreply.github.com>
2025-01-08 15:39:28 -0800

    Update provider.tf

--- azure_users/provider.tf
+++ azure_users/provider.tf
@@ -8,17 +8,15 @@ terraform {
 }
 
 provider "azurerm" {
+  features {}
     subscription_id = var.subscription_id
     tenant_id = var.tenant_id
     client_id = var.client_id
-    client_secret = var.client_secret
-    features {}
+    use_oidc = true
 }
 
-provider "random" {}
+# provider "random" {}
 
 provider "azuread" {
   tenant_id = var.tenant_id
-  client_id = var.client_id
-  client_secret = var.client_secret
 }
