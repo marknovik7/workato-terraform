3e22a07c60cf1ed608b216fbcca9db1419f5935c
Yash Dixit <yash.dixit@workato.com>
2025-01-08 17:35:52 +0530

    terraform ec2 linux & windows

--- aws/ec2/opa/windows-aws/get-latest-windows-ami.tf
+++
@@ -1,8 +0,0 @@
-data "aws_ami" "windows-2022" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "name"
-    values = ["Windows_Server-2022-English-Full-Base*"]
-  }
-}
--- aws/ec2/opa/windows-aws/scripts/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-RELEASE_INFO=`curl --silent https://workato-public.s3.amazonaws.com/agent/release.json`
-VERSION=`echo $RELEASE_INFO | grep -Eo '^\{(\s*,*\s*\"[^"]+\"\s*:\s*\"[^"]+\")+' | grep -Eo '\"version\"\s*:\s*\"[^"]+\"' | grep -Eo '\"[^"]+\"$' | cut -d '"' -f 2` 
-RELEASE_ARTIFACT_URL=`echo $RELEASE_INFO | grep -Eo '\"artifacts\"\s*:\s*\{(\s*,*\s*\"[^"]+\"\s*:\s*\"[^"]+\")+' | grep -Eo '\"linux\"\s*:\s*\"[^"]+\"'| grep -Eo '\"[^"]+\"$' | cut -d '"' -f 2`
-curl -o /tmp/agent.tar.gz $RELEASE_ARTIFACT_URL
-sudo tar xvzf /tmp/agent.tar.gz -C /opt
-rm -rf /tmp/agent.tar.g
