ac78cd0ae3c76795b52b093b3d420b71dfadc2bc
Mark Grivnin <144285053+marknovik7@users.noreply.github.com>
2025-01-07 20:38:33 -0800

    Add files via upload

---
+++ azure_users/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = ">= 4.1.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    subscription_id = var.subscription_id
+    tenant_id = var.tenant_id
+    client_id = var.client_id
+    client_secret = var.client_secret
+    features {}
+}
+
+provider "random" {}
+
+provider "azuread" {
+  tenant_id = var.tenant_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+}
---
+++ azure_users/variables.tf
@@ -0,0 +1,23 @@
+variable "new_user" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+  sensitive = true
+}
+
+variable "tenant_id" {
+  type = string
+  sensitive = true
+}
+
+variable "client_id" {
+  type = string
+  sensitive = true
+}
+
+variable "client_secret" {
+  type = string
+  sensitive = true
+}
