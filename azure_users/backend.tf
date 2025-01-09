5b4c5038055735e80f2e8775defa355550b66a23
Mark Grivnin <144285053+marknovik7@users.noreply.github.com>
2025-01-08 19:05:42 -0800

    Update backend.tf

--- azure_users/backend.tf
+++ azure_users/backend.tf
@@ -6,6 +6,5 @@ terraform {
     key = "terraform.tfstate"
     use_azuread_auth = true
     use_oidc = true
-    access_key = "9o4mRnqThI2AWB0wNxoVL77i0cQYblevrA7VmEUHoZVoca+q9/v5oypizosk4GBPxya9W6SQBNTC+AStexjYqQ=="
   }
 }
