abeceb879d4d67e269e35f9826709ec8064c2949
Mark Grivnin <144285053+marknovik7@users.noreply.github.com>
2025-01-08 19:07:50 -0800

    Update backend.tf

--- azure_users/backend.tf
+++ azure_users/backend.tf
@@ -1,9 +1,5 @@
 terraform {
   backend "azurerm" {
-    resource_group_name = "Admin"
-    storage_account_name = "marktfstate"
-    container_name = "tfstate"
-    key = "terraform.tfstate"
     use_azuread_auth = true
     use_oidc = true
   }
